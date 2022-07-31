99205e679e8e448f585a5f1189c21bfeb6e3067a
Anusha <anushavysakh232@gmail.com>
2022-07-31 20:53:03 +0530

    Uc4 committed

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,34 +1,14 @@
 package com.assignment13.bridglabz;
 
-public class StringMaximum {
-
-	public String maximum(String x, String y, String z) {
-
-		String max = x;
-
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		return max;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Generics Maximum Program");
-		
-		StringMaximum obj = new StringMaximum();
-		
-		System.out.println("Test case1\n Maximum of Apple, Peach, Banana");
-		System.out.println(obj.maximum("Peach","Apple","Banana"));
-		
-		System.out.println("Test case2\n Maximum of Apple, Orange, Banana  ");
-		System.out.println(obj.maximum("Apple","Orange","Banana"));
-		
-		System.out.println("Test case3\n Maximum of Apple, Banana, Strawberry");
-		System.out.println(obj.maximum("Apple","Banana","Strawberry"));
-	}
+public class Refractor1 {
+    
+	public static <T extends Comparable<T>> T testMax(T x, T y, T z){
+        T max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
 
 }
